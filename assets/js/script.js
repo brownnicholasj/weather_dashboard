f97701b9f30fa7a2c43b77bd2c3bae7f09c837fe
Nicholas Brown <brown.nicholasj@gmail.com>
2021-04-14 22:24:49 -0500

    Got connection to weather site and tested that it is returning values

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,29 @@
 //Button listener for search
 var searchBtn = document.querySelector('#searchBtn');
+var currDay = document.querySelector('#currentDay');
+var weatherAPI = '8b5e2ad6d72ea564603e9e5c823217fd';
 
 searchBtn.addEventListener('click', function (event) {
 	event.preventDefault;
 	event.stopPropagation;
 	console.log('search button pressed');
+	var cityName = document.getElementById('cityInput').value;
+	console.log(cityName);
+
+	var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${weatherAPI}`;
+
+	fetch(requestUrl)
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(`Data Whole view below`);
+			console.log(data);
+			console.log(data.length);
+			var listCity = document.createElement('span');
+			listCity.textContent = data.name;
+			currDay.append(listCity);
+		});
 });
 
 var pastCities = [
