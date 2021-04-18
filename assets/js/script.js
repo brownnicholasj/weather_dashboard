7f21c82bb69161a23bd707927e3b5be2f0386ed5
Nicholas Brown <brown.nicholasj@gmail.com>
2021-04-17 23:30:21 -0500

    rendering forecast data on both current and viewing working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //Button listener for search
 var searchBtn = document.querySelector('#searchBtn');
 var currDay = document.querySelector('#currentDay');
+var forecastDays = document.querySelector('#fcContainer');
 var weatherAPI = '8b5e2ad6d72ea564603e9e5c823217fd';
 var today = dayjs().format('MM_DD_YYYY H');
 var todayFormatted = dayjs().format('MM/DD/YYYY HH:MM');
@@ -11,6 +12,7 @@ var currentCityLookup = [];
 searchBtn.addEventListener('click', function (event) {
 	event.preventDefault;
 	event.stopPropagation;
+
 	// console.log('search button pressed');
 	var cityName = document.getElementById('cityInput').value;
 	// console.log(cityName);
@@ -53,35 +55,35 @@ searchBtn.addEventListener('click', function (event) {
 				current_icon: data2.current.weather[0].icon,
 				current_weather: data2.current.weather[0].main,
 				day1_date: data2.daily[1].dt,
-				day1_temp: data2.daily[1].temp,
+				day1_temp: data2.daily[1].temp.day,
 				day1_wind: data2.daily[1].wind_speed,
 				day1_humid: data2.daily[1].humidity,
 				day1_uvi: data2.daily[1].uvi,
 				day1_icon: data2.daily[1].weather[0].icon,
 				day1_weather: data2.daily[1].weather[0].main,
 				day2_date: data2.daily[2].dt,
-				day2_temp: data2.daily[2].temp,
+				day2_temp: data2.daily[2].temp.day,
 				day2_wind: data2.daily[2].wind_speed,
 				day2_humid: data2.daily[2].humidity,
 				day2_uvi: data2.daily[2].uvi,
 				day2_icon: data2.daily[2].weather[0].icon,
 				day2_weather: data2.daily[2].weather[0].main,
 				day3_date: data2.daily[3].dt,
-				day3_temp: data2.daily[3].temp,
+				day3_temp: data2.daily[3].temp.day,
 				day3_wind: data2.daily[3].wind_speed,
 				day3_humid: data2.daily[3].humidity,
 				day3_uvi: data2.daily[3].uvi,
 				day3_icon: data2.daily[3].weather[0].icon,
 				day3_weather: data2.daily[3].weather[0].main,
 				day4_date: data2.daily[4].dt,
-				day4_temp: data2.daily[4].temp,
+				day4_temp: data2.daily[4].temp.day,
 				day4_wind: data2.daily[4].wind_speed,
 				day4_humid: data2.daily[4].humidity,
 				day4_uvi: data2.daily[4].uvi,
 				day4_icon: data2.daily[4].weather[0].icon,
 				day4_weather: data2.daily[4].weather[0].main,
 				day5_date: data2.daily[5].dt,
-				day5_temp: data2.daily[5].temp,
+				day5_temp: data2.daily[5].temp.day,
 				day5_wind: data2.daily[5].wind_speed,
 				day5_humid: data2.daily[5].humidity,
 				day5_uvi: data2.daily[5].uvi,
@@ -106,8 +108,44 @@ searchBtn.addEventListener('click', function (event) {
 			// }
 			searchHistory();
 			currentCityLookup = detailWeather;
+			clearCurrent('uList');
 			// console.log(currentCityLookup);
 			viewForecast();
+			view5day(
+				currentCityLookup.day1_date,
+				currentCityLookup.day1_icon,
+				currentCityLookup.day1_temp,
+				currentCityLookup.day1_wind,
+				currentCityLookup.day1_humid
+			);
+			view5day(
+				currentCityLookup.day2_date,
+				currentCityLookup.day2_icon,
+				currentCityLookup.day2_temp,
+				currentCityLookup.day2_wind,
+				currentCityLookup.day2_humid
+			);
+			view5day(
+				currentCityLookup.day3_date,
+				currentCityLookup.day3_icon,
+				currentCityLookup.day3_temp,
+				currentCityLookup.day3_wind,
+				currentCityLookup.day3_humid
+			);
+			view5day(
+				currentCityLookup.day4_date,
+				currentCityLookup.day4_icon,
+				currentCityLookup.day4_temp,
+				currentCityLookup.day4_wind,
+				currentCityLookup.day4_humid
+			);
+			view5day(
+				currentCityLookup.day5_date,
+				currentCityLookup.day5_icon,
+				currentCityLookup.day5_temp,
+				currentCityLookup.day5_wind,
+				currentCityLookup.day5_humid
+			);
 		});
 });
 
@@ -141,46 +179,84 @@ function viewForecast() {
 	ulEl.setAttribute('id', 'uList');
 
 	currDay.append(ulEl);
-	// console.log(Object.keys(currentCityLookup).length);
-	for (var i = 0; i < Object.keys(currentCityLookup).length; i++) {
-		// console.log(Object.keys(currentCityLookup)[i]);
-		if (Object.keys(currentCityLookup)[i] === `current_temp`) {
-			var ilEl = document.createElement('il');
-			ilEl.setAttribute('id', Object.keys(currentCityLookup)[i]);
-			ilEl.textContent = Object.keys(currentCityLookup)[i];
-			ulEl.append(ilEl);
+	// Append temp
+	currentTempAttr(`current_temp`, 'Temp', currentCityLookup.current_temp, 'F');
+	currentTempAttr(
+		`current_wind`,
+		'Wind',
+		currentCityLookup.current_wind,
+		'MPH'
+	);
+	currentTempAttr(
+		`current_humid`,
+		'Humidity',
+		currentCityLookup.current_humid,
+		'%'
+	);
+	currentTempAttr(
+		`current_uvi`,
+		'UV Index',
+		currentCityLookup.current_uvi,
+		'_'
+	);
+
+	function currentTempAttr(param, label, output, uom) {
+		var pEl = document.createElement('p');
+		var spanEl = document.createElement('span');
+		spanEl.innerHTML = uom;
+		pEl.setAttribute('id', param);
+		if (param === 'current_uvi') {
+			if (output <= 2) {
+				spanEl.setAttribute('class', 'uvIndexGreen');
+			} else if (output <= 5) {
+				spanEl.setAttribute('class', 'uvIndexYellow');
+			} else {
+				spanEl.setAttribute('class', 'uvIndexRed');
+			}
+		}
+		pEl.innerHTML = `${label}: ${output} `;
+		pEl.append(spanEl);
+		ulEl.append(pEl);
+	}
+}
+
+//clear currentView
+function clearCurrent(elementid) {
+	var removeEl = document.getElementById(elementid);
+	if (removeEl !== null) {
+		var count = removeEl.childElementCount;
+		while (removeEl.firstChild) {
+			removeEl.removeChild(removeEl.firstChild);
 		}
 	}
 }
 
+// populate 5-day forecast
+function view5day(date, icon, temp, wind, humid) {
+	var h4El = document.createElement('h4');
+	var pEl = document.createElement('p');
+	var tempEl = document.createElement('p');
+	var windEl = document.createElement('p');
+	var humidEl = document.createElement('p');
+	var divEl = document.createElement('div');
+	var ulEl = document.createElement('ul');
+	ulEl.setAttribute('id', 'uList');
+	var calendar = dayjs.unix(date).format('MM/DD/YYYY');
+	var weatherIcon = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="weather icon">`;
+
+	ulEl.innerHTML = weatherIcon;
+	pEl.textContent = calendar;
+	ulEl.prepend(pEl);
+	tempEl.textContent = `Temp: ${temp} F`;
+	ulEl.append(tempEl);
+	windEl.textContent = `Wind: ${wind} MPH`;
+	ulEl.append(windEl);
+	humidEl.textContent = `Humidity: ${humid} %`;
+	ulEl.append(humidEl);
+	divEl.setAttribute('id', 'forecastBox');
+	divEl.append(ulEl);
+	forecastDays.append(divEl);
+}
+
 // Populate the search history at load of page
 searchHistory();
-
-// var listCity = data.name;
-// currDay.children[0].innerHTML = listCity;
-
-// var ulEl = document.createElement('ul');
-// var tempEL = document.createElement('li');
-
-// ulEl.setAttribute('id', 'uList');
-// currDay.appendChild(ulEl);
-// var currentTemp = {
-// 	temp: data.main.temp,
-// 	wind: data.wind.speed,
-// 	humid: data.main.humidity,
-//UV Found from Long/Lat call needs to be put in
-// };
-
-//Stored in array on CurrentTemp -- need to for loop to create li's
-
-// var temp = data.main.temp;
-// var wind = data.wind.speed;
-// var humid = data.main.humidity;
-// var uv = data;
-// ulEl.append(tempEL);
-// ulEl.children[0].textContent = temp;
-// ulEl.append(tempEL);
-// ulEl.children[1].textContent = wind;
-// ulEl.append(tempEL);
-// ulEl.children[2].textContent = humid;
-// console.log(temp);
